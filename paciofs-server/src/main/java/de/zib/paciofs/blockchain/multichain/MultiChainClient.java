b9a84efaa05b6e81f6b0fc91f18514662f569694
Robert Schmidtke <ro.schmidtke@gmail.com>
2019-01-28 17:28:15 +0100

    refactor starting and stopping of multichaind

--- paciofs-server/src/main/java/de/zib/paciofs/blockchain/multichain/MultiChainClient.java
+++ paciofs-server/src/main/java/de/zib/paciofs/blockchain/multichain/MultiChainClient.java
@@ -84,113 +84,100 @@ public class MultiChainClient extends BitcoinJSONRPCClient {
   // manages the transition from RUNNING -> STOPPING -> STOPPED
   @Override
   public void stop() {
-    if (this.state != State.RUNNING) {
-      // if chain is STARTING: let the chain start first before stopping
-      // if chain is STOPPING: some other thread is already stopping the chain
-      // if chain is STOPPED: well, we are good then
-    } else {
-      // have exactly one thread do the stopping
-      synchronized (this) {
-        if (this.state == State.RUNNING) {
-          this.state = State.STOPPING;
-        } else {
-          return;
-        }
-      }
-
-      if (this.multiChaind != null) {
-        // seems more reliable: we get an exit code, and the RPC stop request fails since the server
-        // does not answer anymore (since it is shutting down)
-        this.multiChaind.terminate();
-      } else {
-        // TODO do we shut down remote chains? this should be prohibited by proper authorization
-        super.stop();
-      }
-
-      // done
-      this.state = State.STOPPED;
+    if (!swapState(State.RUNNING, State.STOPPING)) {
+      // we are not the thread that does the stopping
+      return;
     }
+
+    if (this.multiChaind != null) {
+      // seems more reliable: we get an exit code, and the RPC stop request fails since the server
+      // does not answer anymore (since it is shutting down)
+      this.multiChaind.terminate();
+    } else {
+      // TODO do we shut down remote chains? this should be prohibited by proper authorization
+      super.stop();
+    }
+
+    // done
+    this.state = State.STOPPED;
   }
 
   // manages the transition from STOPPED -> STARTING -> RUNNING
   private void ensureRunning() {
-    if (this.state != State.STOPPED) {
-      // if chain is STARTING: nothing we can do except wait (see below)
-      // if chain is RUNNING: well, we are good then
-      // if chain is STOPPING: let the chain stop first before starting
-    } else {
-      // have exactly one thread do the starting
-      synchronized (this) {
-        if (this.state == State.STOPPED) {
-          this.state = State.STARTING;
-        } else {
-          return;
-        }
-      }
-
-      // wait until the service is up
-      final long startWait = System.currentTimeMillis();
-      this.multiChaind.start();
-
-      // the code multichain responds with while warming up
-      final int multiChaindWarmupCode = this.config.getInt(MultiChainOptions.WARMUP_CODE_KEY);
-
-      // wait at most backoff * (2^maxRetries - 1) milliseconds
-      // e.g. 50 * (2^10 -1) = 51150 milliseconds
-      long backoff = this.config.getLong(MultiChainOptions.BACKOFF_MILLISECONDS);
-      final int maxRetries = this.config.getInt(MultiChainOptions.BACKOFF_RETRIES);
-      int failedRetries = 0;
-      BitcoinRPCException lastCaught = null;
-      for (; failedRetries < maxRetries; ++failedRetries) {
-        try {
-          final BlockChainInfo bci = this.getBlockChainInfo();
-          LOG.debug("Connected to chain {}", bci.chain());
-          break;
-        } catch (BitcoinRPCException rpcException) {
-          final BitcoinRPCError rpcError = rpcException.getRPCError();
-
-          if (rpcError != null && rpcError.getCode() == multiChaindWarmupCode) {
-            LOG.debug(
-                "Waiting {} ms, multichaind is warming up ({})", backoff, rpcError.getMessage());
-
-            // keep waiting, multichaind is at work and will be with us soon
-            --failedRetries;
-          } else {
-            // multichaind might have crashed
-            if (!this.multiChaind.isRunning()) {
-              this.state = State.STOPPED;
-              throw new RuntimeException("multichaind stopped running");
-            } else {
-              lastCaught = rpcException;
-              LOG.debug("Waiting {} ms, multichaind has not started yet ({})", backoff,
-                  rpcException.getMessage());
-              LOG.debug(Markers.EXCEPTION, "multichaind has not started yet", rpcException);
-            }
-          }
-
-          // exponential backoff so we do not annoy multichaind too much
-          try {
-            Thread.sleep(backoff);
-            backoff *= 2;
-          } catch (InterruptedException e) {
-            if (LOG.isDebugEnabled(Markers.EXCEPTION)) {
-              LOG.debug(Markers.EXCEPTION, "Interrupted while waiting for multichaind to start", e);
-            } else {
-              LOG.debug("Interrupted while waiting for multichaind to start: {}", e.getMessage());
-            }
-          }
-        }
-      }
-
-      final long waitTime = System.currentTimeMillis() - startWait;
-      if (failedRetries == maxRetries) {
-        throw new RuntimeException("multichaind not up after " + waitTime + " ms", lastCaught);
-      } else {
-        LOG.debug("multichaind up after {} ms", waitTime);
-      }
-
-      // done
-      this.state = State.RUNNING;
+    if (!swapState(State.STOPPED, State.STARTING)) {
+      // we are not the thread that does the starting
+      return;
     }
+
+    // wait until the service is up
+    this.multiChaind.start();
+
+    // the code multichain responds with while warming up
+    final int multiChaindWarmupCode = this.config.getInt(MultiChainOptions.WARMUP_CODE_KEY);
+
+    // wait at most backoff * (2^maxRetries - 1) milliseconds
+    // e.g. 50 * (2^10 -1) = 51150 milliseconds
+    long backoff = this.config.getLong(MultiChainOptions.BACKOFF_MILLISECONDS);
+    final int maxRetries = this.config.getInt(MultiChainOptions.BACKOFF_RETRIES);
+    int failedRetries = 0;
+    for (; failedRetries < maxRetries; ++failedRetries) {
+      try {
+        final BlockChainInfo bci = this.getBlockChainInfo();
+        LOG.debug("Connected to chain {}", bci.chain());
+        break;
+      } catch (BitcoinRPCException rpcException) {
+        final BitcoinRPCError rpcError = rpcException.getRPCError();
+
+        if (rpcError != null && rpcError.getCode() == multiChaindWarmupCode) {
+          LOG.debug(
+              "Waiting {} ms, multichaind is warming up ({})", backoff, rpcError.getMessage());
+
+          // keep waiting, multichaind is at work and will be with us soon
+          --failedRetries;
+        } else {
+          // multichaind might have crashed
+          if (!this.multiChaind.isRunning()) {
+            this.state = State.STOPPED;
+            throw new RuntimeException("multichaind stopped running");
+          }
+
+          LOG.debug("Waiting {} ms, multichaind has not started yet ({})", backoff,
+              rpcException.getMessage());
+          LOG.debug(Markers.EXCEPTION, "multichaind has not started yet", rpcException);
+        }
+
+        // exponential backoff so we do not annoy multichaind too much
+        try {
+          Thread.sleep(backoff);
+          backoff *= 2;
+        } catch (InterruptedException e) {
+          if (LOG.isDebugEnabled(Markers.EXCEPTION)) {
+            LOG.debug(Markers.EXCEPTION, "Interrupted while waiting for multichaind to start", e);
+          } else {
+            LOG.debug("Interrupted while waiting for multichaind to start: {}", e.getMessage());
+          }
+        }
+      }
+    }
+
+    if (failedRetries == maxRetries) {
+      throw new RuntimeException("multichaind not up after " + failedRetries + " retries");
+    }
+
+    LOG.debug("multichaind up after {} retries", failedRetries);
+
+    // done
+    this.state = State.RUNNING;
+  }
+
+  private boolean swapState(State from, State to) {
+    synchronized (this) {
+      if (this.state == from) {
+        this.state = to;
+        return true;
+      }
+    }
+
+    return false;
   }
 }

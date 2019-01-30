91690e3a56776c131f7eb18d3f6a68c34dc516be
Robert Schmidtke <ro.schmidtke@gmail.com>
2019-01-30 18:44:37 +0100

    format Java test sources as well

--- paciofs-server/format-sources.sh
+++ paciofs-server/format-sources.sh
@@ -4,4 +4,5 @@
 # until a .clang-format file is found and uses it. .clang-format files are
 # located in the subdirectories passed to `find`.
 find "$(pwd)/src/main/java"  -name '*.java'  | xargs clang-format -style=file -i
+find "$(pwd)/src/test/java"  -name '*.java'  | xargs clang-format -style=file -i
 find "$(pwd)/src/main/proto" -name '*.proto' | xargs clang-format -style=file -i

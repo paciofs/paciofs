3eda30819806fbe9af7326faaa9ef143d9faea3c
Robert Schmidtke <ro.schmidtke@gmail.com>
2019-02-02 15:43:34 +0100

    add log-level option

--- paciofs-docker/docker-compose-minikube.sh
+++ paciofs-docker/docker-compose-minikube.sh
@@ -3,13 +3,19 @@
 usage() {
   echo "Usage: $0 [options]"
   echo "Options:"
+  echo "  --log-level (default: not specified)"
   echo "  --no-clean (default: not specified)"
 }
 
+COMPOSE_OPTIONS=""
 BUILD_OPTIONS=""
 while [[ $# -gt 0 ]]; do
   key="$1"
   case $key in
+    --log-level)
+      COMPOSE_OPTIONS="${COMPOSE_OPTIONS} --log-level $2"
+      shift
+      ;;
     --no-cache)
       BUILD_OPTIONS="${BUILD_OPTIONS} --no-cache"
       ;;
@@ -42,4 +48,5 @@ eval $(minikube docker-env)
 docker-compose --host "${DOCKER_HOST}" \
   --tls --tlscacert "${DOCKER_CERT_PATH}/ca.pem" --tlscert "${DOCKER_CERT_PATH}/cert.pem" --tlskey "${DOCKER_CERT_PATH}/key.pem" --tlsverify \
   --file "${DIR}/docker-compose.yaml" \
+  ${COMPOSE_OPTIONS} \
   build ${BUILD_OPTIONS}

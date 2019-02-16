51e5ccafe74404bb7a0ef20fd732bdae50d8d8b0
Robert Schmidtke <ro.schmidtke@gmail.com>
2019-02-16 23:54:02 +0100

    add basic test skeleton

---
+++ paciofs-csi/src/github.com/paciofs/paciofs/paciofs-csi/pfs/driver_test.go
@@ -0,0 +1,11 @@
+package pfs
+
+import (
+	"testing"
+	// "github.com/kubernetes-csi/csi-test/pkg/sanity"
+)
+
+func TestDriver(t *testing.T) {
+	// config := &sanity.Config{}
+	// sanity.Test(t, config)
+}

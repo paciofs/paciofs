ed417e291ba6497f8daf9413844cde370265adce
Robert Schmidtke <ro.schmidtke@gmail.com>
2019-07-05 17:19:57 +0200

    create source package for OBS from small distribution

--- paciofs-obs/create_source_tarball.sh
+++ paciofs-obs/create_source_tarball.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 
 paciofs_version=$1
-if [ -z "$1" ]; then
+if [[ -z "${paciofs_version}" ]]; then
   echo "No version given, aborting"
   exit 1
 fi
 
-cd ..
+os=$(uname)
+if [[ "${os}" == "Linux" ]]; then
+  readlink_cmd=readlink
+elif [[ "${os}" == "Darwin" ]]; then
+  readlink_cmd=greadlink
+fi
+current_dir=$(dirname $(${readlink_cmd} -f $0))
 
 # the directory we will put the necessary files in to build on OBS
-DIST_DIR=$(mktemp -d -t paciofs.XXXXXX)
-echo "Building distribution in ${DIST_DIR}"
+dist_dir=${current_dir}/dist
+rm -rf ${dist_dir}
+echo "Building distribution in ${dist_dir}"
 
-# the OBS build VMs are offline, so copy necessary dependencies to distribution
-mkdir ${DIST_DIR}/maven-repository
-mvn --file ./paciofs-client/pom.xml \
-  --activate-profiles docker \
-  package org.apache.maven.plugins:maven-dependency-plugin:3.1.1:go-offline \
-  --define maven.repo.local=${DIST_DIR}/maven-repository \
-  --define skipTests=true
-
-# create lean distribution
-./paciofs-docker/make_dist.sh ${DIST_DIR}
+# create lean distribution like in docker
+${current_dir}/../paciofs-docker/make_dist.sh ${dist_dir}
 
 # clone third parties into the distribution
-mvn --file ${DIST_DIR}/paciofs-client/third_party/pom.xml initialize
+mvn --file ${dist_dir}/paciofs-client/third_party/pom.xml initialize
 
-DIST_ARCHIVE=$(pwd)/paciofs-obs/paciofs_${paciofs_version}.orig.tar.gz
+# the OBS build VMs are offline, so copy necessary dependencies to distribution
+mkdir ${dist_dir}/maven-repository
+mvn --file ${dist_dir}/pom.xml \
+  --projects paciofs-client,paciofs-server \
+  --activate-profiles docker \
+  org.apache.maven.plugins:maven-dependency-plugin:3.1.1:go-offline \
+  --define maven.repo.local=${dist_dir}/maven-repository \
+  --define skipTests=true
 
-cd ${DIST_DIR}
-tar czf ${DIST_ARCHIVE} \
-  pom.xml \
-  paciofs-client \
-  maven-repository
+# remove all .git folders
+git_files=$(find ${dist_dir} -name '.git')
+for git_file in ${git_files}; do
+  rm -rf ${git_file}
+done
 
-rm -rf ${DIST_DIR}
+# package the whole thing
+dist_archive=${current_dir}/paciofs_${paciofs_version}.orig.tar.gz
+
+# change to parent directory of dist_dir so we can have relative paths inside the archive
+tar czf ${dist_archive} -C $(dirname ${dist_dir}) $(basename ${dist_dir})
+
+rm -rf ${dist_dir}

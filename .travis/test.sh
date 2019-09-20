ddd88f7d3f9957cad6939d597e1f179823c28ac4
Robert Schmidtke <ro.schmidtke@gmail.com>
2019-09-20 10:16:09 +0200

    cleanup test script, add server restart test case

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-os=$(uname)
-if [[ "${os}" == "Linux" ]]; then
-  umount_cmd=fusermount
-  umount_options=-u
-elif [[ "${os}" == "Darwin" ]]; then
-  umount_cmd=diskutil
-  umount_options=unmount
-fi
+function stop_paciofs_server() {
+  local paciofs_server_pid=$1
+  kill "${paciofs_server_pid}"
+  wait "${paciofs_server_pid}"
+  local exit_code=$?
+
+  # exit code for programs that are sent SIGTERM
+  test "143" -eq "${exit_code}"
+}
+
+function create_file_system() {
+  local file_system_name="$1"
+  ./paciofs-client/target/Release/mkfs.paciofs localhost:8080 "${file_system_name}" -d TRACE
+}
+
+function mount_file_system() {
+  local file_system_name="$1"
+  local mount_point="$2"
+  mkdir -p "${mount_point}"
+  ./paciofs-client/target/Release/mount.paciofs localhost:8080 "${mount_point}" "${file_system_name}" -o default_permissions -o fsname=paciofs -o noatime -d TRACE &
+  sleep 5s
+}
+
+function unmount_file_system() {
+  local mount_point="$1"
+
+  os=$(uname)
+  if [[ "${os}" == "Linux" ]]; then
+    fusermount -u "${mount_point}"
+  elif [[ "${os}" == "Darwin" ]]; then
+    diskutil unmount "${mount_point}"
+  fi
+}
 
 # generate TLS certificates
 cd ./paciofs-server/src/test/scripts && ./gen-certs.sh && cd ../../../../
@@ -15,16 +40,32 @@ cd ./paciofs-server/src/test/scripts && ./gen-certs.sh && cd ../../../../
 # run tests
 mvn test --batch-mode
 
-# start PacioFS server
+# setup
 mvn --projects paciofs-server exec:java@run-server &
-mvn_pid=$!
+paciofs_server_pid=$!
 sleep 60s
 
-# create and mount file system
-./paciofs-client/target/Release/mkfs.paciofs localhost:8080 volume1 -d TRACE
-mkdir -p /tmp/mnt-volume1
-./paciofs-client/target/Release/mount.paciofs localhost:8080 /tmp/mnt-volume1 volume1 -o default_permissions -o fsname=paciofs -o noatime -d TRACE &
-sleep 5s
+create_file_system "volume1"
+mount_file_system "volume1" "/tmp/mnt-volume1"
+
+# create directory and empty file in it
+mkdir /tmp/mnt-volume1/dir
+touch /tmp/mnt-volume1/dir/file.empty
+sync
+
+# stop and delete data directory
+unmount_file_system "/tmp/mnt-volume1"
+stop_paciofs_server "${paciofs_server_pid}"
+rm -rf /tmp/paciofs-data-dir
+
+# restart server
+mvn --projects paciofs-server exec:java@run-server &
+paciofs_server_pid=$!
+sleep 60s
+
+# creation of volumes, directories and empty files should survive server restart even when losing the data directory
+mount_file_system "volume1" "/tmp/mnt-volume1"
+test -f /tmp/mnt-volume1/dir/file.empty
 
 # write file
 dd if=/dev/urandom of=/tmp/file.rnd bs=1048576 count=10
@@ -33,19 +74,12 @@ time cp /tmp/file.rnd /tmp/mnt-volume1/file.rnd
 sync
 
 # remount to clear caches
-${umount_cmd} ${umount_options} /tmp/mnt-volume1
-./paciofs-client/target/Release/mount.paciofs localhost:8080 /tmp/mnt-volume1 volume1 -o default_permissions -o fsname=paciofs -o noatime -d TRACE &
-sleep 5s
+unmount_file_system "/tmp/mnt-volume1"
+mount_file_system "volume1" "/tmp/mnt-volume1"
 
 # files should not differ
 diff /tmp/file.rnd /tmp/mnt-volume1/file.rnd; diff_exit=$?
 test "0" -eq "${diff_exit}"
 
-# unmount
-${umount_cmd} ${umount_options} /tmp/mnt-volume1
-
-# wait for everyting to shut down
-kill ${mvn_pid} && wait ${mvn_pid}; mvn_exit=$?
-
-# JVM return code when sent SIGTERM
-test "143" -eq "${mvn_exit}"
+unmount_file_system "/tmp/mnt-volume1"
+stop_paciofs_server "${paciofs_server_pid}"
